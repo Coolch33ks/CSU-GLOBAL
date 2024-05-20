b97b47b3ff585af12ac6942b9fb29732234eaf38
coolch33ks <lainezstacey@gmail.com>
2024-05-20 00:12:18 -0400

    CSU

---
+++ src/CT/CT1.java
@@ -0,0 +1,12 @@
+package CT;
+
+public class CT1 {
+	public static void main(String args[]) {
+		System.out.println("Cheeks");
+		System.out.println("Lainez");
+		System.out.println("123 Rainbow Rd");
+		System.out.println("Clouds");
+		System.out.println("12345");
+
+	}
+}
